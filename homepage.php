2c1804956744f720e8f5169a28762196273a15d1
Bram Steur <220507@student.glu.nl>
2023-12-11 11:37:56 +0100

    portfolio

---
+++ homepage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <title>Porftfolio</title>
+</head>
+<body>
+    <header>
+        <h1 id="intro">Welkom bij:</h1>
+        <h1 class="ml11">
+            <span class="text-wrapper">
+            <span class="line line1"></span>
+            <span class="name">Bram&nbsp;Studios</span>
+            </span>
+        </h1>
+        <!-- <h1 id="name">Bram Studios</h1> -->
+        <div class="taskbar">
+            <h1 class="special">Specialismes</h1>
+            <div class="html">
+                <h2 class="code">HTML</h2>
+            </div>
+            <div class="css">
+                <h2 class="code">CSS</h2>
+            </div>
+            <div class="wordpress"></div>
+            <div class="database"></div>
+        </div>
+    </header>
+    <body>
+        
+    </body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    <script src="assets/js/animation.js"></script>
+</body>
+</html>
