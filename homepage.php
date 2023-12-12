03feb20907bf2b0c356be3df6ff75eb78c9f1f7b
Bram Steur <220507@student.glu.nl>
2023-12-12 15:53:06 +0100

    newheader

--- homepage.php
+++ homepage.php
@@ -8,15 +8,22 @@
 </head>
 <body>
     <header>
-        <h1 id="intro">Welkom bij:</h1>
-        <h1 class="ml11">
-            <span class="text-wrapper">
-            <span class="line line1"></span>
-            <span class="name">Bram&nbsp;Studios</span>
-            </span>
-        </h1>
-        <!-- <h1 id="name">Bram Studios</h1> -->
-        <div class="taskbar">
+        <div id="frontpage">
+         <h1 id="intro">Welkom bij:</h1>
+            <h1 class="ml11">
+                <span class="text-wrapper">
+                <span class="line line1"></span>
+                <span class="name">Bram&nbsp;Studios</span>
+                </span>
+            </h1>
+    	</div>
+        <!-- <img id="headerimg" src="assets/img/bramwedstrijd.jpg" alt="bram"> -->
+        <div class="headerimg">
+            <img src="assets/img/bramwedstrijd.jpg" alt="bram">
+        </div>
+    </header>
+
+    <div class="taskbar">
             <h1 class="special">Specialismes</h1>
             <div class="html">
                 <h2 class="code">HTML</h2>
@@ -27,10 +34,6 @@
             <div class="wordpress"></div>
             <div class="database"></div>
         </div>
-    </header>
-    <body>
-        
-    </body>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
     <script src="assets/js/animation.js"></script>
 </body>
