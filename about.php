dba86d479fa5323038f2d4ff6de750f4d598b3f1
Bram Steur <220507@student.glu.nl>
2023-12-18 09:04:41 +0100

    scroll

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+    <div id="navbarabout">
+        <h1>Bram Studios: About</h1>
+        <nav>
+        <a href="homepage.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="#projects">Projects</a>
+    </nav>
+    </div>
+<body>
+    
+</body>
+</html>
--- homepage.php
+++ homepage.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Porftfolio</title>
 </head>
 <body>
@@ -23,18 +23,34 @@
         </div>
     </header>
 
-    <div class="taskbar">
-            <h1 class="special">Specialismes</h1>
-            <div class="html">
-                <h2 class="code">HTML</h2>
-            </div>
-            <div class="css">
-                <h2 class="code">CSS</h2>
-            </div>
-            <div class="wordpress"></div>
-            <div class="database"></div>
-        </div>
+    <div id="navbar" style="bottom: 0;">
+        <a href="homepage.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="#projects">Projects</a>
+    </div>
+
+
+    <div class="bram">
+        <h1>Wie ben ik?</h1>
+    </div>
+    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
     <script src="assets/js/animation.js"></script>
+    <script>
+      window.onscroll = function () {
+        scrollNavFromBottom();
+      };
+      var screenSize = window.innerHeight - 40;
+      function scrollNavFromBottom() {
+        var bottomOffset = document.documentElement.scrollTop;
+        if (screenSize > bottomOffset) {
+          document.getElementById("navbar").style.bottom = bottomOffset + "px";
+        }
+        if (bottomOffset >= screenSize) {
+          document.getElementById("navbar").style.bottom = screenSize + "px";
+        }
+      }
+    </script>
 </body>
 </html>
